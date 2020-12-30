5cfd31115612fbe670bf74d401e33a1f63cec422
Samuel Wölfl <samuelwoelfl@gmx.de>
2020-12-30 14:42:16 +0100

    Beautified the code

--- smooth_scroll.js
+++ smooth_scroll.js
@@ -1,3 +1,7 @@
 $('body').localScroll();
 $('#nav-drawer').localScroll();
+$('#goto-top-link a').localScroll();
+$('#goto-top-link > a').localScroll();
+$('#goto-top-link .btn').localScroll();
+$('.btn-light').localScroll();
 $('.list-group-item').localScroll();
